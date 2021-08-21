8e4a5dcc9e8f0e628e834b4bb5c147d3d467c017
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-08-21 10:37:15 +0900

    [add]comment_sample

---
+++ comment_sample.rb
@@ -0,0 +1,13 @@
+=begin
+「楽しいRuby　第５版」
+=end
+
+= 10　#横
+y = 20 #縦
+z = 30 #高さ
+#表面積と体積を計算
+area = (x*y + y*z + z*x) * 2
+volume = x * y * z
+#出力
+print "表面積=", area, "\n"
+print "体積=", volume, "\n"

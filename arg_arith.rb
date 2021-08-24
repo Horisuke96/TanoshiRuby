eee39562a8c6c91e48657dfea32e8c71d6aec7b4
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-08-24 17:17:57 +0900

    [add]arg_arith

---
+++ arg_arith.rb
@@ -0,0 +1,10 @@
+#arithmetic
+#数値に変換して計算
+
+num0 = ARGV[0].to_i
+num1 = ARGV[1].to_i
+
+puts "#{num0} + #{num1} = #{num0 + num1}"
+puts "#{num0} - #{num1} = #{num0 - num1}"
+puts "#{num0} * #{num1} = #{num0 * num1}"
+puts "#{num0} / #{num1} = #{num0 / num1}"

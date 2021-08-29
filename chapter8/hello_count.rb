775b5eb19c9af02a668f266fd21d0ba4c74ad71a
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-08-29 23:24:54 +0900

    [fix]hello_count

--- chapter8/hello_count.rb
+++ chapter8/hello_count.rb
@@ -9,3 +9,18 @@ class HelloCount
     @name = myname
   end
   
+  def hello
+    @@count += 1
+    puts "Hello, World. I am #{@name}.\n"
+  end
+end
+
+bob = HelloCount.new("Bob")
+alice = HelloCount.new("Alice")
+ruby = HelloCount.new
+
+p HelloCount.count
+bob.hello
+alice.hello
+ruby.hello
+p HelloCount.count

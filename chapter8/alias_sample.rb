b753aa72461e770a1f876eb739c81fc711e4fc30
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-08-31 15:22:36 +0900

    [add]alias_test_car

--- chapter8/alias_sample.rb
+++ chapter8/alias_sample.rb
@@ -5,7 +5,7 @@ class C1
 end
 
 class C2 < C1
-  alias :old_hello :hello　#別名のold_helloを設定
+  alias old_hello hello　#別名のold_helloを設定
 
   def hello  #再定義
     "#{old_hello}, again"
@@ -15,3 +15,6 @@ end
 obj = C2.new
 p obj.old_hello
 p obj.hello
+
+
+#Traceback (most recent call last):1: from alias_sample.rb:7:in `<main>'alias_sample.rb:8:in `<class:C2>': undefined method `hello　' for class `C2' (NameError)
---
+++ chapter8/alias_test_car.rb
@@ -0,0 +1,31 @@
+class Car
+  def accele
+    print("アクセルを踏みました\n")
+  end
+
+  def brake
+    print("ブレーキを踏みました\n")
+  end
+end
+
+class Soarer < Car
+  def openRoof
+    print("ルーフを開けました\n")
+  end
+end
+
+class Crown < Car
+  alias brake! brake    #brakeからbrake!へ
+
+  def reclining
+    print("シートをリクライニングしました\nß")
+  end
+end
+
+soarer = Soarer.new
+soarer.openRoof
+soarer.accele
+
+crown = Crown.new
+crown.reclining
+crown.brake!

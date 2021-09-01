0d05a06f991b03f1f9e87465dc9dd05f478bd029
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-09-01 23:06:20 +0900

    [add]teamlab4

---
+++ sample/teamlab4.rb
@@ -0,0 +1,26 @@
+# 1-30までを出力し、3の倍数と３のつく数字は☆をつける
+
+def findthird
+  num = 1
+  sum = 0
+  while (num <= 40000) do
+    if (num % 3 == 0 || num.to_s.include?("3"))
+      #3の倍数とは、数字を3で割りき切れる数字なので「%3」
+      #3のつく数字はnumの数を文字列に変換し、「to_s」、
+      #指定した文字列"3"が含まれている場合にtrueを返す条件を加える。「.include?("3")」
+      puts "#{ num } ☆"
+      
+      sum = sum + num
+    else
+      puts num
+    end
+
+    
+
+    num = num + 1
+  end
+
+  puts sum
+end
+
+findthird
